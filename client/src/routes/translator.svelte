43b38906f557a9815cd03fb2e68306d4134a750f
madpeh <48161600+madpeh@users.noreply.github.com>
2022-05-18 18:29:05 +0200

    Remove console log

--- client/src/routes/translator.svelte
+++ client/src/routes/translator.svelte
@@ -26,14 +26,13 @@
     });
     if (response.ok) {
       const responseBody = await response.json();
-      console.log(responseBody)
       const translatorVersion = responseBody['translator_version'];
       translatedCode = `# translator version: ${translatorVersion}\n`;
       responseBody['agent_code_lines'].forEach((codeLine) => {
         translatedCode += codeLine;
       });
       if (responseBody['graph_code_lines'].length) {
-        translatedCode += '\n# graph generation\n'
+        translatedCode += '\n# graph generation\n';
         responseBody['graph_code_lines'].forEach((codeLine) => {
           translatedCode += codeLine;
         });
