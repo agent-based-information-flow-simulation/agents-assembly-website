3040a90ba71f6b4eff1686be50273f1624d70072
madpeh <48161600+madpeh@users.noreply.github.com>
2022-05-18 14:00:55 +0200

    Add nav bar wrapping

--- client/src/components/nav.svelte
+++ client/src/components/nav.svelte
@@ -1,4 +1,4 @@
-<nav class="flex justify-center w-full">
+<nav class="flex justify-center w-full flex-wrap">
   <a class="mx-4 text-lg hover:underline" href="/">Home</a>
   <a class="mx-4 text-lg hover:underline" href="/translator">Translator</a>
   <a class="mx-4 text-lg hover:underline" href="/docs">Documentation</a>
