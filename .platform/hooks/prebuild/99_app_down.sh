f434b66603cce15bdf8d0da19c724caf9647ddf7
madpeh <48161600+madpeh@users.noreply.github.com>
2022-05-19 22:19:13 +0200

    Disable prebuild script

--- .platform/hooks/prebuild/99_app_down.sh
+++ .platform/hooks/prebuild/99_app_down.sh
@@ -7,11 +7,11 @@
 # i.e. t2.small or AWS CodePipeline (additional costs)
 # this solution introduces a tiny downtime during the build process
 
-DOCKER_PROCESS_COUNT=`docker ps -q | wc -l`
-if [ "$DOCKER_PROCESS_COUNT" -gt 0 ]; then docker stop --time 30 $(docker ps -q); fi
+#DOCKER_PROCESS_COUNT=`docker ps -q | wc -l`
+#if [ "$DOCKER_PROCESS_COUNT" -gt 0 ]; then docker stop --time 30 $(docker ps -q); fi
 
 # if this script returns a non-zero value then the instance loops itself
 # trying to run the prebuild scripts indefinitely
 # so if 'docker stop' fails then the build process should continue and
 # potentially fail later due to having not enough memory
-exit 0
+#exit 0
