2019f84aba51b23e90b1564510aee5d73a0078c2
madpeh <48161600+madpeh@users.noreply.github.com>
2023-05-22 18:33:03 +0200

    Fix typo

--- client/src/routes/about.svelte
+++ client/src/routes/about.svelte
@@ -10,7 +10,7 @@
 <div class="flex flex-col items-center ml-0 mr-0 md:ml-10 md:mr-10">
   <p>We do not store any data.</p>
   <p class="text-center">
-    The source code of all Agents Assembly releated applications can be found <a
+    The source code of all Agents Assembly related applications can be found <a
       class="text-blue-800 hover:underline"
       href="https://github.com/agent-based-information-flow-simulation">here</a
     >.
